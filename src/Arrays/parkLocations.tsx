96707cdc479af5ada642d22eb0a286029735a0cf
Ehrlacker <101287793+Ehrlacker@users.noreply.github.com>
2023-10-31 18:38:09 -0400

    add drawRouteOnMap and handleOptimizeRoute functions

--- src/Arrays/parkLocations.tsx
+++ src/Arrays/parkLocations.tsx
@@ -4,15 +4,15 @@ import { parkProps } from "../types"
 const parkLocations: parkProps[] = [
 {
     name: "Apex Community Park",
-    coordinates: [35.754939, -78.827544] as [number, number]
+    coordinates: [ -78.827544,35.754939] as [number, number]
 },
 {
     name: "Jaycee Park",
-    coordinates: [35.715810, -78.840994] as [number, number]
+    coordinates: [ -78.864545,35.730186] as [number, number]
 },
 {
     name: "Brown Center",
-    coordinates: [35.734131, -78.846481] as [number, number]
+    coordinates: [-78.846430,35.733697] as [number, number]
 }
 
 ]
